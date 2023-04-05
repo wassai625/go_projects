6d2e21b5818188c3b838d40c4d4a8c6e00ffae97
wassai625 <98636488+wassai625@users.noreply.github.com>
2023-04-05 11:23:51 +0900

    feature_add_createUser

--- todo_app/go.mod
+++ todo_app/go.mod
@@ -3,6 +3,7 @@ module todo_app
 go 1.20
 
 require (
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
--- todo_app/main.go
+++ todo_app/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"fmt"
-	"log"
+	// "log"
 	"todo_app/app/models"
-	"todo_app/config"
+	// "todo_app/config"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+	// log.Println("test")
 	fmt.Println(models.Db)
+
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
+
 }
