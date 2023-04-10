5eaff4612bdafec6e8f5b0a789a7a10ce67eaf98
wassai625 <98636488+wassai625@users.noreply.github.com>
2023-04-10 14:57:59 +0900

    feature_add_updateUser

--- todo_app/main.go
+++ todo_app/main.go
@@ -14,14 +14,24 @@ func main() {
 	// fmt.Println(config.Config.LogFile)
 
 	// log.Println("test")
-	fmt.Println(models.Db)
+	// fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
+
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
+
 	fmt.Println(u)
 
-	u.CreateUser()
+	u.Name = "test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 
 }
