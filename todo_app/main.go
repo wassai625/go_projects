0b1355181a0990c199cd8b32695d602b2088adfa
wassai625 <98636488+wassai625@users.noreply.github.com>
2023-04-13 11:11:13 +0900

    add_db_users_table

--- todo_app/main.go
+++ todo_app/main.go
@@ -14,7 +14,7 @@ func main() {
 	// fmt.Println(config.Config.LogFile)
 
 	// log.Println("test")
-	// fmt.Println(models.Db)
+	fmt.Println(models.Db)
 
 	// u := &models.User{}
 	// u.Name = "test"
@@ -31,6 +31,10 @@ func main() {
 	u.Name = "test2"
 	u.Email = "test2@example.com"
 	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	u.DeleteUser()
 	u, _ = models.GetUser(1)
 	fmt.Println(u)
 
