566c0640f30fa9cff3d365c7af73185d98647515
wassai625 <98636488+wassai625@users.noreply.github.com>
2023-04-17 15:53:35 +0900

    add_view

--- todo_app/app/controllers/server.go
+++ todo_app/app/controllers/server.go
@@ -7,6 +7,9 @@ import (
 )
 
 func StartMainServer() error {
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
+
 	http.HandleFunc("/", top)
 	handler1 := func(w http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, "Hello-1\n")
